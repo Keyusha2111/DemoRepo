edffb162f1304c6b01937e8d35587626ccc7e86f
Keyusha Patel <keyushapatel2111@gmail.com>
2022-03-17 14:03:16 -0400

    added driver initialization part

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -1,12 +1,17 @@
 package com.qa.pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class HomePage {
 
 	public void homePage() {
 		System.out.println("Inside home page");
 	}
 	
-	public HomePage() {
+	WebDriver driver;
+	
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
 		
 	}
 }
